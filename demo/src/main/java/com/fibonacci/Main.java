fbb5f5d2790a69adf11b5386035c380883d258c3
Njabulo <njsiban022@student.wethinkcode.co.za>
2023-12-25 09:06:26 +0200

    fibonacci series works perfectly.

--- demo/src/main/java/com/fibonacci/Main.java
+++ demo/src/main/java/com/fibonacci/Main.java
@@ -4,16 +4,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         while (true) {
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Number of terms for fibonacci series: ");
-            int nTerms = scan.nextInt();
-            if (nTerms <=0){System.out.println("Term value must be > 0.");continue;}
-            else{
-                int numTerms = numSeries(nTerms);
-                
-            } 
+                Scanner scan = new Scanner(System.in); 
+                System.out.print("Number of terms for fibonacci series: ");
+                int nTerms = scan.nextInt();
+                if (nTerms <=0){System.out.println("Term value must be > 0.");continue;}
+                else{
+                    int n1 = 0;
+                    int n2 = 1;
+                    System.out.println("First "+nTerms+" fibonacci numbers are: ");
+                    System.out.print(n1+" "+n2);
+                    int numTerms = numSeries(nTerms);
+                    for(int i = 0;i<numTerms;i++){
+                        int n3 = n1+n2;
+                        System.out.print(" "+n3);
+                        n1 = n2;
+                        n2 = n3;
+                    }
+                    System.out.println();
+                    scan.close();
+                    break;
+
+
+                }
+            }
         }
-    }
 
 
     static int numSeries(int terms){
