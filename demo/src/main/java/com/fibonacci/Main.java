f8d5a97e6daeeec19e90d87fd4040dd968f7f4db
Njabulo <njsiban022@student.wethinkcode.co.za>
2023-12-25 10:52:28 +0200

    Still working on test cases.

--- demo/src/main/java/com/fibonacci/Main.java
+++ demo/src/main/java/com/fibonacci/Main.java
@@ -3,35 +3,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-                Scanner scan = new Scanner(System.in); 
-                System.out.print("Number of terms for fibonacci series: ");
-                int nTerms = scan.nextInt();
-                if (nTerms <=0){System.out.println("Term value must be > 0.");continue;}
-                else{
-                    int n1 = 0;
-                    int n2 = 1;
-                    System.out.println("First "+nTerms+" fibonacci numbers are: ");
-                    System.out.print(n1+" "+n2);
-                    int numTerms = numSeries(nTerms);
-                    for(int i = 0;i<numTerms;i++){
-                        int n3 = n1+n2;
-                        System.out.print(" "+n3);
-                        n1 = n2;
-                        n2 = n3;
-                    }
-                    System.out.println();
-                    scan.close();
-                    break;
-
-
-                }
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Number of terms for fibonacci series: ");
+            int nTerms = scan.nextInt();
+            int n1 = 0;
+            int n2 = 1;
+            System.out.println("First "+Math.abs(nTerms)+" fibonacci numbers are: ");
+            System.out.print(n1+" "+n2);
+            int numTerms = numSeries(nTerms);
+            for(int i = 0;i<numTerms;i++){
+                int n3 = n1+n2;
+                System.out.print(" "+n3);
+                n1 = n2;
+                n2 = n3;
             }
+            System.out.println();
+            scan.close();
         }
 
 
-    static int numSeries(int terms){
-        return terms;
-
+    public static int numSeries(int term){
+        if(term <=0){
+            return Math.abs(term);
+        }
+        return term;
     }
 }
---
+++ demo/src/test/java/Test.java
@@ -0,0 +1,5 @@
+package demo.src.test.java;
+
+public @interface Test {
+
+}
